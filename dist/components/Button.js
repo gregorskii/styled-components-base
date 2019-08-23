9469a26e08cd949c2b7a42b9d24ff84565a58a63
Gregory Parsons <gregorskii@gmail.com>
2019-08-23 16:58:36 -0700

    theme fix

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -7,7 +7,9 @@ exports["default"] = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -16,6 +18,7 @@ var Button = _styledComponents["default"].button.withConfig({
   displayName: "Button",
   componentId: "sc-1v1frjm-0"
 })(["color:", ";border:2px solid ", ";background:", ";font-size:1em;margin:1em;padding:0.25em 1em;border-radius:3px;"], function (props) {
+  console.log("blah", props);
   return props.theme.fg;
 }, function (props) {
   return props.theme.fg;
