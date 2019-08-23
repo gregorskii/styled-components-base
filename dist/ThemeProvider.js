9f78e2aee8a07119bbc2dca6dfbe4db105e84e35
Gregory Parsons <gregorskii@gmail.com>
2019-08-23 16:47:13 -0700

    theme fix

--- dist/ThemeProvider.js
+++ dist/ThemeProvider.js
@@ -13,17 +13,10 @@ var _styledComponents = require("styled-components");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 var ThemeProvider = function ThemeProvider(props) {
-  var theme = _objectSpread({
+  var theme = {
     base: _theme.theme
-  }, props.theme);
-
+  };
   return _react["default"].createElement(ThemeProvider, {
     theme: theme
   }, props.children);
