ba983432ec93c2783098125e72d03495d46e0c42
Gregory Parsons <gregorskii@gmail.com>
2019-08-23 17:16:49 -0700

    theme fix

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -16,7 +16,6 @@ var Button = _styledComponents["default"].button.withConfig({
   displayName: "Button",
   componentId: "sc-1v1frjm-0"
 })(["color:", ";border:2px solid ", ";background:", ";font-size:1em;margin:1em;padding:0.25em 1em;border-radius:3px;"], function (props) {
-  console.log("blah", props);
   return props.theme.fg;
 }, function (props) {
   return props.theme.fg;
